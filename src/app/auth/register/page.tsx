96775ccfc432261505a7e51e3a088ec2ffd83e98
oyerohabib <oyerohabib@yahoo.com>
2024-07-23 11:54:47 +0100

    feat: created InputField component, and completed register page

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,76 +1,70 @@
 import Link from "next/link";
 import Image from "next/image";
 import DevLinkLogo from "/public/images/devLinkLogo.svg";
+import { AiOutlineMail } from "react-icons/ai";
+import InputField from "@/app/components/InputField";
 
 export default function Signup() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
+      <div className="w-full max-w-[476px]">
         <div className="flex items-center gap-2 justify-center mb-6">
           <Image src={DevLinkLogo} width={183} height={40} alt="Devlink logo" />
         </div>
-        <h2 className="text-4xl font-bold">Create account</h2>
-        <h3 className="font-normal text-base text-grey">
-          Let’s get you started sharing your links!
-        </h3>
-        <form className="mt-8 space-y-6">
-          <div className="rounded-md shadow-sm">
-            <div>
-              <label htmlFor="email-address" className="">
-                Email address
-              </label>
-              <input
-                id="email-address"
+        <div className="bg-white rounded-lg p-8 space-y-6">
+          <h2 className="text-4xl font-bold">Create account</h2>
+          <h3 className="font-normal text-base text-grey">
+            Let’s get you started sharing your links!
+          </h3>
+          <form className="mt-8 space-y-6">
+            <div className="rounded-md shadow-sm">
+              <InputField
+                id="email"
                 name="email"
                 type="email"
                 required
-                className="block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                label="Email address"
                 placeholder="Email address"
+                icon={AiOutlineMail}
               />
-            </div>
-            <div className="mt-4">
-              <label htmlFor="password" className="">
-                Create password
-              </label>
-              <div className="relative flex items-center gap-3">
-                <span className="absolute">icon</span>
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  required
-                  className="block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                  placeholder="Password"
-                />
-              </div>
-            </div>
-            <div className="mt-4">
-              <label htmlFor="confirm-password" className="">
-                Confirm password
-              </label>
-              <input
+              <InputField
+                id="password"
+                name="password"
+                type="password"
+                required
+                label="Create password"
+                placeholder="At least 8 characters"
+                icon={AiOutlineMail}
+              />
+
+              <InputField
                 id="confirm-password"
                 name="confirm-password"
                 type="password"
                 required
-                className="block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                label="Confirm password"
                 placeholder="Confirm Password"
+                icon={AiOutlineMail}
               />
             </div>
+            <p className="text-grey text-xs font-normal">
+              Password must contain at least 8 characters
+            </p>
+            <div>
+              <button
+                type="submit"
+                className="w-full py-2 text-white font-semibold text-base h-[46px] p-[11px_27px] bg-indigo-600 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Create new account
+              </button>
+            </div>
+          </form>
+          <div className="flex justify-center mt-4 text-base font-normal">
+            <span className="text-grey">Already have an account?&nbsp;</span>
+            <Link href="/login" className="text-purple hover:underline">
+              Login
+            </Link>
           </div>
-          <div>
-            <button
-              type="submit"
-              className="w-full py-2 text-white bg-indigo-600 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-              Sign Up
-            </button>
-          </div>
-        </form>
-        <div className="flex justify-center mt-4">
-          <Link href="/login" className="text-indigo-600 hover:underline">
-            Already have an account? Login
-          </Link>
         </div>
       </div>
     </div>
---
+++ src/app/components/InputField.tsx
@@ -0,0 +1,48 @@
+import { IconType } from "react-icons";
+
+interface InputFieldProps {
+  id: string;
+  name: string;
+  type: string;
+  required?: boolean;
+  placeholder?: string;
+  label?: string;
+  icon?: IconType;
+}
+
+const InputField: React.FC<InputFieldProps> = ({
+  id,
+  name,
+  type,
+  required = false,
+  placeholder = "",
+  label = "",
+  icon: Icon,
+}) => {
+  return (
+    <div className="mt-4">
+      {label && (
+        <label htmlFor={id} className="text-xs text-dark-grey">
+          {label}
+        </label>
+      )}
+      <div className="relative flex items-center gap-3">
+        {Icon && (
+          <span className="absolute left-4">
+            <Icon />
+          </span>
+        )}
+        <input
+          id={id}
+          name={name}
+          type={type}
+          required={required}
+          className="block w-full py-3 px-10 border border-borders rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+          placeholder={placeholder}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default InputField;
