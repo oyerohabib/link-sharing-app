ea388fe5aa758bb71522218d2e167d8c49ff220b
oyerohabib <oyerohabib@yahoo.com>
2024-07-24 03:28:06 +0100

    fix: improved navbar component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import DevLinkLogo from "/public/images/devLinkLogo.svg";
+import { usePathname } from "next/navigation";
+import { IoPersonCircleOutline } from "react-icons/io5";
+import { FaLink } from "react-icons/fa6";
 
 export default function Header() {
+  const pathname = usePathname();
   return (
     <header className="flex items-center justify-between p-4 bg-white shadow-md">
       <div className="flex items-center">
@@ -14,17 +18,37 @@ export default function Header() {
           className="mr-2"
         />
       </div>
-      <div className="flex space-x-4">
-        <Link href="/" className="text-gray-700">
+      <div className="flex items-center space-x-4 transition duration-200">
+        <Link
+          href="/"
+          className={`flex items-center gap-2 font-semibold text-base ${
+            pathname === "/"
+              ? "p-[11px_27px] bg-light-purple rounded-lg text-purple"
+              : "text-grey"
+          }`}
+        >
+          <span>
+            <FaLink />
+          </span>
           Links
         </Link>
-        <Link href="/profile-detail" className="text-gray-700">
+        <Link
+          href="/profile-detail"
+          className={`flex items-center gap-2 font-semibold text-base ${
+            pathname === "/profile-detail"
+              ? "p-[11px_27px] bg-light-purple rounded-lg text-purple"
+              : "text-grey"
+          }`}
+        >
+          <span>
+            <IoPersonCircleOutline />
+          </span>
           Profile Details
         </Link>
       </div>
       <Link
         href="/preview"
-        className="px-4 py-2 text-white bg-purple-500 rounded"
+        className="px-4 py-2 text-purple text-base font-semibold bg-purple-500 rounded-lg border border-purple"
       >
         Preview
       </Link>
